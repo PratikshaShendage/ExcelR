aee5d19b2eafda5d91606d30305a244ed049d2d4
Pratiksha Shendage <pratikshashendage4131@gmail.com>
2025-04-01 22:17:18 +0530

    upload day8 task

---
+++ java ExcelR/p3.java
@@ -0,0 +1,22 @@
+class p3{
+	public static void main(String args[]){
+	
+		System.out.println("10");
+		
+		System.out.println("20");
+		
+		System.out.print("30");
+		
+		System.out.print("40");
+	}
+}
+
+
+
+// single line comment
+/* multiline comment 
+// in java we have two types of printing statements
+// System.out.println("20"); --> new line
+// System.out.print("20"); ---> out in same line no next line
+/*  name()   */
+      
