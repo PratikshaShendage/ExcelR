6ce2c4732152978258f947fedfdefbd61a6ea3a2
Pratiksha Shendage <pratikshashendage4131@gmail.com>
2025-04-01 13:14:26 +0530

    day7 file upload

---
+++ java ExcelR/My.java
@@ -0,0 +1,10 @@
+class My{
+ public static void main(String args[]){
+ 
+ int a=10;
+ 
+ System.out.println("hii welcome to java");
+ System.out.println("hlo");
+ 
+ }
+}
---
+++ java ExcelR/p2.java
@@ -0,0 +1,51 @@
+
+class p2{
+ public static void main(String args[]){
+ 
+ //block of code
+ 
+   System.out.println("hello world");
+   
+   String val="hello world";
+         int  val2=100;
+         
+         
+    String name= "Pratiksha";
+    int age =22;
+    long phone=462387467;
+    String email="p@gmail";
+   
+   System.out.println("My name is"+ name);
+   System.out.println(age);
+   System.out.println(phone);
+    System.out.println(email);
+   
+   
+ }
+}
+
+
+
+// Notes 2nd days of java excelR
+{
+	//block of code
+	
+	variable : it is used to store data.
+	           it act like a container. where it can store data
+			   
+	
+	Data Types : 2 types
+	it defines type of a data 
+	
+	1 primitive datatype : 
+	  byte
+	  short
+	  int 
+	  long 
+	  float- 100.0
+	  double -100.0
+	  char - 'a'
+	  boolean--true /false
+	  
+	2 non-primitive data type :
+}
